1e40b8ef76dc02c94502b4fc67e3fdba4b0d6344
Viacheslav Glazhevskyi <glajik@gmail.com>
2019-12-17 19:48:15 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -r -f dist/*.js
+find dist/ -name '*.js' -type f -delete
 cp -r -u src/* dist/
 find dist/ -type f -exec sed -i '/module\.exports.*/d;/import.*/d;s/export //g' {} \;
 # npx webpack
